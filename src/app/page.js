47ff5ad028166dfc2af9ff0624d46f106b566ec8
guilhermetrutaa <guilhermetrutaa@gmail.com>
2025-05-01 09:20:57 -0300

    :bug: Fix: Arrumando Bugs

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 
 
-const page = () => {
+const Page = () => {
   const router = useRouter()
 
   return (
@@ -58,4 +58,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
