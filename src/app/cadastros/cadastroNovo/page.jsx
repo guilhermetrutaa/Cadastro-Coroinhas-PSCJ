927c7006ec6e4355ec30af6e648d55df8d74ef88
guilhermetrutaa <guilhermetrutaa@gmail.com>
2025-05-01 17:24:41 -0300

    Fix: Arrumando Bugs

--- src/app/cadastros/cadastroNovo/page.jsx
+++ src/app/cadastros/cadastroNovo/page.jsx
@@ -36,7 +36,7 @@ export default function Home() {
   return (
     <div className="w-full min-h-screen bg-[#f9f9f9] flex flex-col items-center">
       <div className="pt-20 text-center">
-        <p className="text-[#000] text-[2rem] font-semibold pb-2">Cadastro Novo</p>
+        <p className="text-[#000] text-[2rem] font-semibold pb-2">Cadastro Novato</p>
         <p className="text-[#000] max-w-[20rem] pb-6 mx-auto">
           Preencha os campos abaixo para realizar seu cadastro em nosso sistema
         </p>
